0889986b3962324f71d3a519eeda4438582f93a9
MirkoSisko <mirko.sisko@blankhq.co>
2022-08-01 15:22:05 +0200

    Deployed last version, updated constants

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ module.exports = {
     goerli: {
       url: "https://eth-goerli.g.alchemy.com/v2/EBa2mJHdCf_aI272FZFk5KHFGr6DroFg",
       // If using main wallet DO NOT share this info
+      // accounts: process.env.NEXT_PUBLIC_METAMASK_KEY,
       accounts: [
         "70dcb1918708ee2ad96e9d9ef633ad9ca5f35373214c71d9a6132f86eeace915",
       ],
